5ae1a3979cad87041aa03b2b1e4d411589153463
5s-clef <zjw930908@gmail.com>
2016-11-01 09:55:26 +0800

    Update p-c_folk.c

--- p-c_folk.c
+++ p-c_folk.c
@@ -18,28 +18,33 @@
 int outfd,infd;
 int pfd[2];
 
-
+/*定义传输数据结构data struct
+ *ioffset->文件偏移量
+ *ilen->文件长度
+ *buf->存储文件数据
+ */
 struct data{
 	int ioffset;
 	int ilen;
 	char buf[BLOCKSIZE];
 };
-
+/*写入进程执行函数*/
 void fork_w(){
 int fork_num;
 int n = 0;
 struct data *tempdataptr;
-close(pfd[1]);
+close(pfd[1]);//关闭写入进程的管道写端口，如果没关read中将会被阻塞
 for(;;){
 	
 	
 	tempdataptr = malloc(sizeof(struct data));
-	
+	 /*从管道中读取整个struct出来*/
 	if((n = read(pfd[0],tempdataptr,sizeof(struct data))) < 0){
 		printf("read pipe error\n");
 	}
 	
-	else if (n == 0){	
+	else if (n == 0){
+	/*如果读取的字段长度为0，则说明文件已全部读完，管道被关闭，进程结束*/
 		printf("write complete\n");
 		break;
 	}
@@ -53,24 +58,31 @@ for(;;){
 	
 }
 }
-
+/*读取进程执行函数*/
 void fork_r(int i){
 int fork_num = i;
 int n = 0;
 struct data *tempdataptr;
-close(pfd[0]);
+close(pfd[0]);//关闭读取进程的管道读端口
 for(;;){
 	
 	tempdataptr = malloc(sizeof(struct data));
+	/*从文件中读取整个struct并将其写入管道
+	 *进程的编号fork_num，读取数据时根据编号分段读取
+	 *例如有5个进程，则0号进程读0,5,10段，1号读1,6,11段，以此类推
+	 */
 	if((n = pread(infd,tempdataptr->buf,BLOCKSIZE,BLOCKSIZE*fork_num)) < 0)
 		printf("read infd error\n");
 	else if (n == 0){
+	/*如果读取的字段长度为0，则说明已经读到了文件末尾，进程结束*/
 		printf("%d\n",BLOCKSIZE*(fork_num));
 		free(tempdataptr);
 		printf("read %d complete\n",i);
 		break;
 	}
 	else{
+	/*将读取的文件信息存入struct结构中，并将进程编号相应增加，以在下次循环中 读取下一段内容
+	 */
 		tempdataptr->ioffset = BLOCKSIZE*fork_num;
 		tempdataptr->ilen = n;
 		write(pfd[1],tempdataptr,sizeof(struct data));
@@ -80,17 +92,14 @@ for(;;){
 	
 }
 }
-
-int main(int argc,char *argv[]){
+/*主函数*/
+int main(int argc,char *argv[]){ 
 
 clock_t start_time,finish_time;
 double total_time;
 start_time = clock();
 
 int i,err;
-//pid_t wpid[NFORK];
-//pid_t rpid[NFORK];
-//int fork_state_w[NFORK],fork_state_r[NFORK];
 int fork_num,fork_state[NFORK],status[NFORK];
 
 if(argc != 3){
@@ -107,34 +116,40 @@ if(infd < 0){
 	printf("open infile.txt error!\n");
 	return -1;
 }
+/*创建管道*/
 if(pipe(pfd)<0){
 	printf("create pipe error!\n");
 	return -1;
 }
 
-
+/*创建进程
+ *此处只有父进程能够创建子进程，父进程创建完后进入等待，子进程进入读写函数 
+ */
 for(i = 0; i < NFORK; i++){
 	status[i] = fork();
-	fork_num = i;	//记录进程数
+	fork_num = i;	//记录进程数，由于创建进程后数据分离，每个进程的fork_num数都不同
 	if(status[i] == 0 || status[i] == -1) break;	//确保只有父进程创建子进程
 	else
 		fork_num = -1;	
 }
+/*读进程函数*/
 if(fork_num < NFORK/2 && fork_num >= 0)
 	fork_r(fork_num);
+/*写进程函数*/
 if(fork_num >= NFORK/2 && fork_num >= 0)
 	fork_w();
-if(fork_num < 0){ //父进程等待
+/*父进程等待子进程执行*/
+if(fork_num < 0){ 
 	for(i = 0; i < NFORK/2; i++){
 		waitpid(status[i],&fork_state[i],0);
-	}//判断写入进程完成
+	}//判断读取进程完成
+	/*关闭最后一个管道写端口，此时read函数不再阻塞*/
 	close(pfd[1]);
 	printf("pipe1 closed\n");
 		
 	for(;i < NFORK; i++){
-		//close(pfd[1]);
 		waitpid(status[i],&fork_state[i],0);
-	}
+	}//判断写入进程完成
 }
 close(infd);
 close(outfd);
